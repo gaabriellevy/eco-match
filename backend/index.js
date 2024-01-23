e925cbedd52cd69f322103dd01e1a60d3a0d59cf
Gabriel Levy <levygabriel11@gmail.com>
2024-01-23 15:08:11 -0300

    configuração do sequelize

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,10 @@
-const port = 3335
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const Router = require('./routes');
 
-const express = require("express") // importa o framework express
-const db = require("./database") // importa o banco de dados
+app.use(cors());
+app.use(express.json());
+app.use(Router);
 
-const app = express() // instancia o express
-
-// const itemController = require('./controllers/item') // tudo relacionado ao item é controlado por essa classe para fim de abstração
-
-app.use(express.json()) // transforma a requisição em formato json para objeto javascript
-
-db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`)) // sincroniza com o banco de dados
-
-app.listen(port, () => { // escuta as requisições na porta declarada
-    console.log(`Servidor iniciado na porta ${port}`)
-})
+app.listen(3333)
