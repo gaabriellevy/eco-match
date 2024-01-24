d5110807b9fd7714180db2f2158563f8fd7a7984
Gabriel Levy <levygabriel11@gmail.com>
2024-01-24 13:41:44 -0300

    aprimora produtoController

--- backend/controllers/produtoController.js
+++ backend/controllers/produtoController.js
@@ -1,28 +1,24 @@
-const db = require('../models');
-const Produto = db.Produto;
-
-const listarProdutos = async (req, res) => {
-    try {
-        const produtos = await Produto.findAll();
-        res.json(produtos);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao listar produtos.' });
-    }
-};
-
-const criarProduto = async (req, res) => {
-    try {
-        const { nome, descricao, preco } = req.body;
-        const novoProduto = await Produto.create({ nome, descricao, preco });
-        res.status(201).json(novoProduto);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao criar produto.' });
-    }
-};
+const { Produto } = require('../models');
 
 module.exports = {
-    listarProdutos,
-    criarProduto,
-};
+    async index (req, res) {
+        try {
+            const produtos = await Produto.findAll();
+            res.json(produtos);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao listar produtos.' });
+        }
+    },
+
+    async store (req, res) {
+        try {
+            const { nome, descricao, preco } = req.body;
+            const novoProduto = await Produto.create({ nome, descricao, preco });
+            res.status(201).json(novoProduto);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao criar produto.' });
+        }
+    }
+}
