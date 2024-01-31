eb7b8b9e5330d52f28738eec361169c0e5476396
Gabriel Levy <levygabriel11@gmail.com>
2024-01-30 22:51:09 -0300

    corrige migration de produto

--- backend/migrations/20240124164953-create-produto.js
+++ backend/migrations/20240124164953-create-produto.js
@@ -2,7 +2,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('Produto', {
+        await queryInterface.createTable('produtos', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
             nome: {
                 type: Sequelize.STRING
             },
@@ -23,6 +29,6 @@ module.exports = {
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Produto');
+        await queryInterface.dropTable('produtos');
     }
 };
